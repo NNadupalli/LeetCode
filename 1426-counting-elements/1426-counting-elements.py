c159e7aa4e7ae6b4904f026f030fe87fbe80d218
Nalini Nadupalli <nalini.hogwarts@gmail.com>
2023-03-28 17:38:26 -0400

    Time: 37 ms (72.37%), Space: 13.8 MB (90.48%) - LeetHub

--- 1426-counting-elements/1426-counting-elements.py
+++ 1426-counting-elements/1426-counting-elements.py
@@ -1,14 +1,20 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
         
-        arr_set = set(arr)
+        arr.sort()
         count = 0
+        run_length = 1
         
-        for a in arr:
-            if a+1 in arr_set:
-                count += 1
+        for i in range(len(arr)):
+            if arr[i-1]!= arr[i]:
+                if arr[i-1]+1 == arr[i]:
+                    count += run_length
+                run_length = 0
+            run_length += 1
+        
         return count
         
-#Time complexity : O(n)
-#Space complexity: O(n)
+        
+#Time complexity : O(nlogn)
+#Space complexity: O(n) to O(1) depending on the type of sort algorithm used to implement array sort in python
         
