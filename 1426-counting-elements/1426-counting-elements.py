91fc668facc02db4c9d1fc40c417aefe87d3342e
Nalini Nadupalli <nalini.hogwarts@gmail.com>
2023-03-28 17:28:29 -0400

    Time: 49 ms (18.12%), Space: 13.9 MB (48.95%) - LeetHub

---
+++ 1426-counting-elements/1426-counting-elements.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        
+        arr_set = set(arr)
+        count = 0
+        
+        for a in arr:
+            if a+1 in arr_set:
+                count += 1
+        return count
+        
+
+        
