0927027882f738966a0830274f107c778b91c0ec
Nalini Nadupalli <nalini.hogwarts@gmail.com>
2023-03-28 16:08:51 -0400

    Time: 35 ms (34.59%), Space: 13.9 MB (39.11%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -7,3 +7,6 @@ class Solution:
                 return sa
             Mset.add(sa)
         return 
+
+    #Time complexity = O(n)
+    #Space complexity = O(1) since the string is part of english alphabet which is always 26 letters
