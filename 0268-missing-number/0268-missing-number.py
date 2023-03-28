61034d465fb3bcc68666dd2fef53d84dcd3ed1b8
Nalini Nadupalli <nalini.hogwarts@gmail.com>
2023-03-28 16:29:36 -0400

    Time: 2784 ms (13.12%), Space: 15.2 MB (66.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,7 +5,8 @@ class Solution:
         n = len(nums)
         
         for i in range(n+1):
-            if i not in nums:
+            if i not in nums: 
+        #Time complexity of if condition above is O(n) i.e. checking if an element exists in an array is O(n) and not O(1)     
                 return i
 
             
